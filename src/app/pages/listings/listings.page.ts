c8f5b0e7397cb1fbf8f61a114a2c74580d73ce0e
Prashant Purmah <prashant.purmah@sdworx.com>
2023-01-26 23:52:46 +0100

    Sliding options + filter

--- src/app/pages/listings/listings.page.ts
+++ src/app/pages/listings/listings.page.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { filter } from 'rxjs';
 import { Listing } from 'src/app/interfaces/listing';
+import { User } from 'src/app/interfaces/user';
 import { ListingsService } from 'src/app/services/listings.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-listings',
@@ -12,9 +14,12 @@ import { ListingsService } from 'src/app/services/listings.service';
 export class ListingsPage implements OnInit {
 
   public allListings: Listing[] = [];
+  public showFilteredByAvailableList = true;
+  private users: User[];
 
   constructor(
     private listingsService: ListingsService,
+    private userService: UserService,
     private router: Router
     ) { }
 
@@ -27,11 +32,19 @@ export class ListingsPage implements OnInit {
     });
     console.log('ngOnInit');
     this.setListings();
+    this.userService.getAllUsers().subscribe((users) => this.users = users);
   }
 
-  private setListings() : void {
+  public callSeller(sellerId) {
+    const sellerContactNumber = this.users.find(user => user.id === sellerId)?.contactNumber;
+    window.open(`tel:${sellerContactNumber}`);
+  }
+
+  public setListings() : void {
     this.listingsService.getAllListings().subscribe((listings) => {
-      this.allListings = listings;
+      this.allListings = this.showFilteredByAvailableList 
+        ? listings.filter(l => (l.status.toString()) === 'Available')
+        : listings;
       console.log('all listings:', this.allListings);
     });
   }
